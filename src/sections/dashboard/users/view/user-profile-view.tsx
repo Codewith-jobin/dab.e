047e459e74e414f679f0c5f0de1c9a902b7757da
jobin <jose.jobiin@gmail.com>
2024-10-25 15:24:02 +0530

    commit d

--- src/sections/dashboard/users/view/user-profile-view.tsx
+++ src/sections/dashboard/users/view/user-profile-view.tsx
@@ -1,38 +1,38 @@
-import { Box, Card } from "@mui/material"
-import { ProfileCover } from "../profile-cover"
+// import { Box, Card } from "@mui/material"
+// import { ProfileCover } from "../profile-cover"
 
-export const UserProfileView = () => {
-  return(
-    <Box>
-      USER PROFILE VIEW
+// export const UserProfileView = () => {
+//   return(
+//     <Box>
+//       USER PROFILE VIEW
 
-      <Card sx={{ mb: 3, height: 290 }}>
-        <ProfileCover
-          // role={_userAbout.role}
-          name={'ansaf'}
-          // avatarUrl={user?.photoURL}
-          // coverUrl={_userAbout.coverUrl}
-        />
+//       <Card sx={{ mb: 3, height: 290 }}>
+//         <ProfileCover
+//           // role={_userAbout.role}
+//           name={'ansaf'}
+//           // avatarUrl={user?.photoURL}
+//           // coverUrl={_userAbout.coverUrl}
+//         />
 
-        {/* <Box
-          display="flex"
-          justifyContent={{ xs: 'center', md: 'flex-end' }}
-          sx={{
-            width: 1,
-            bottom: 0,
-            zIndex: 9,
-            px: { md: 3 },
-            position: 'absolute',
-            bgcolor: 'background.paper',
-          }}
-        >
-          <Tabs value={tabs.value} onChange={tabs.onChange}>
-            {TABS.map((tab) => (
-              <Tab key={tab.value} value={tab.value} icon={tab.icon} label={tab.label} />
-            ))}
-          </Tabs>
-        </Box> */}
-      </Card>
-    </Box>
-  )
-}
+//         {/* <Box
+//           display="flex"
+//           justifyContent={{ xs: 'center', md: 'flex-end' }}
+//           sx={{
+//             width: 1,
+//             bottom: 0,
+//             zIndex: 9,
+//             px: { md: 3 },
+//             position: 'absolute',
+//             bgcolor: 'background.paper',
+//           }}
+//         >
+//           <Tabs value={tabs.value} onChange={tabs.onChange}>
+//             {TABS.map((tab) => (
+//               <Tab key={tab.value} value={tab.value} icon={tab.icon} label={tab.label} />
+//             ))}
+//           </Tabs>
+//         </Box> */}
+//       </Card>
+//     </Box>
+//   )
+// }
