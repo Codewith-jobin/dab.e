a69b539c29346167b094cfdb27fdc6d6f484a43a
jobin <jose.jobiin@gmail.com>
2024-10-25 15:19:02 +0530

    commit d

--- src/sections/dashboard/invoice/view/invoice-list-view.tsx
+++ src/sections/dashboard/invoice/view/invoice-list-view.tsx
@@ -1,12 +1,12 @@
-import { Box } from "@mui/material"
-import { InvoiceStatusCards } from "../invoice-status-cards"
+// import { Box } from "@mui/material"
+// import { InvoiceStatusCards } from "../invoice-status-cards"
 
-export const InvoiceListView = () => {
-  return(
-    <Box sx={{px: 5}}>
-      INVOICE LIST VIEW PARENT
+// export const InvoiceListView = () => {
+//   return(
+//     <Box sx={{px: 5}}>
+//       INVOICE LIST VIEW PARENT
 
-      <InvoiceStatusCards />
-    </Box>
-  )
-}
+//       <InvoiceStatusCards />
+//     </Box>
+//   )
+// }
